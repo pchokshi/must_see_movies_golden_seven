c19dfd87a9f033ee099942a043381b41d2631647
pchokshi <poojachokshi@Poojas-MacBook-Air.local>
2015-10-30 10:52:09 -0500

    done with 2

--- app/controllers/golden_controller.rb
+++ app/controllers/golden_controller.rb
@@ -9,4 +9,37 @@ class GoldenController < ApplicationController
   def director_show
     @director = Director.find_by({ :id => params[:id]})
   end
+
+  def director_delete
+    @director_delete = Director.find_by({ :id => params[:id]})
+    @director_delete.destroy
+    redirect_to("http://localhost:3000/directors")
+  end
+
+  def director_new_form
+  end
+
+  def director_create_new
+    p=Director.new
+    p.name = params[:the_name]
+    p.image_url = params[:the_image_url]
+    p.dob = params[:the_dob]
+    p.bio = params[:the_bio]
+    p.save
+
+    redirect_to("http://localhost:3000/directors")
+  end
+  def director_edit_form
+    @director = Director.find_by({ :id => params[:id]})
+  end
+
+  def update_director
+    @director=Director.find_by({ :id => params[:id]})
+    @director.name = params[:the_name]
+    @director.image_url = params[:the_image_url]
+    @director.dob = params[:the_dob]
+    @director.bio = params[:the_bio]
+    @director.save
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,20 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "golden", :action => "home" })
 
+  # Routes to CREATE
+  get("/directors/new_form", {:controller => "golden", :action => "director_new_form"})
+  get("/directors/create_new", {:controller => "golden", :action => "director_create_new"})
+
+  # Routes to READ
   get("/directors", {:controller => "golden", :action => "director_index"})
   get("/directors/:id",       { :controller => "golden", :action => "director_show" })
+
+
+  # Routes to DELETE
+  get("/director_delete/:id", { :controller => "golden", :action => "director_delete" })
+
+  # Routes to EDIT
+  get("/directors/:id/edit", { :controller => "golden", :action => "director_edit_form" })
+  get("/directors/update_director/:id", { :controller => "golden", :action => "update_director" })
+
 end
